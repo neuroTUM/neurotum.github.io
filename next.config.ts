aed706a8ba1a74885a698fab8b10e88ddc18d834
pvlov <Daniel.Pavlov@gmx.de>
2025-10-12 17:18:45 +0200

    try out deploying to gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
     output: "export",
     reactStrictMode: true,
-    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"]
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    basePath: isProd ? "/webpage" : "",
+
+    // FIXME: We should lint our codebase!
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
