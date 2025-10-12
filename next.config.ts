ff8fa76a29b89652650db1fb465c6745616d9977
pvlov <Daniel.Pavlov@gmx.de>
2025-10-12 18:09:32 +0200

    try poking jekyll

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const basePath = "/webpage"
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
     output: "export",
     reactStrictMode: true,
     pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-    basePath: basePath,
-    assetPrefix: basePath,
+    basePath: isProd ? basePath : "",
+    assetPrefix: isProd ? basePath : "",
 
     // FIXME: We should lint our codebase!
     eslint: {
