cb209248c6080cdeec576116a10cfd6e097e16f4
Enrico <5878721+Garfield100@users.noreply.github.com>
2025-10-13 20:55:10 +0200

    add member photos and link files in their structs

--- src/app/components/MEMBERS.tsx
+++ src/app/components/MEMBERS.tsx
@@ -8,317 +8,370 @@ export const MEMBERS: Member[] = [
     team: Team.Electronics,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Electronics/Ekaterina Atanasov.jpg"
   },
   {
     name: "Vivien Hopf",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Neuromotion/Vivien Hopf.jpg"
   },
   {
     name: "Niels Thiemann",
     team: Team.BCI,
     status: "Member",
     studyCourse: "MSc Neuroengineering",
+    imageLink: "/headshots/BCI/Niels Thiemann.jpg"
   },
   {
     name: "Watsa Jukthumrong",
     team: Team.Electronics,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Electronics/Watsa Jukthumrong.jpg"
   },
   {
     name: "Glykeria Rovatsou",
     team: Team.Operations,
     status: "Board Member & Co-Director",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Glykeria Rovatsou.jpg"
   },
   {
     name: "Jens von Greiff",
     team: Team.BCI,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/BCI/Jens von Greiff.jpg"
   },
   {
     name: "Manuel Methasani",
     team: Team.BCI,
     status: "Passive Member",
     studyCourse: undefined,
+    imageLink: "/headshots/BCI/Manuel Methasani.jpg"
   },
   {
     name: "Nicolas Mallinckrodt",
     team: Team.Operations,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Nicolas Mallinckrodt.jpg"
   },
   {
     name: "Iustin Curcean",
     team: Team.BCI,
     status: "Passive Member",
     studyCourse: "-",
+    imageLink: "/headshots/BCI/Iustin Curcean.jpg"
   },
   {
     name: "Levi Frim",
     team: Team.BCI,
     status: "Member",
     studyCourse: "-",
+    imageLink: "/headshots/BCI/Levi Frim.jpg"
   },
   {
     name: "Nick Breit",
     team: Team.Operations,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Nick Breit.jpg"
   },
   {
     name: "Jenny Tabeling",
     team: Team.Operations,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Jenny Tabeling.jpg"
   },
   {
     name: "Ekaterina Semenova",
     team: Team.Neuromotion,
     status: "Team Lead",
     studyCourse: undefined,
+    imageLink: "/headshots/Neuromotion/Ekaterina Semenova.jpg"
   },
   {
     name: "Rania Bannour",
     team: Team.Electronics,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Electronics/Rania Bannour.jpg"
   },
   {
     name: "Sadat Hodzic",
     team: Team.BCI,
     status: "Team Lead",
     studyCourse: "-",
+    imageLink: "/headshots/BCI/Sadat Hodzic.jpg"
   },
   {
     name: "Lucas Mateus Martins Araújo e Castro",
     team: Team.BCI,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/BCI/Lucas Mateus Martins Araújo e Castro.jpg"
   },
   {
     name: "Marina Tazeva",
     team: Team.Operations,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Marina Tazeva.jpg"
   },
   {
     name: "Anita Reutova",
     team: Team.Operations,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Anita Reutova.jpg"
   },
   {
     name: "Muhammed Samir Öztürk",
     team: Team.Operations,
     status: "Passive Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Muhammed Samir Öztürk.jpg"
   },
   {
     name: "Enrico Fazzi",
     team: Team.Electronics,
     status: "Team Lead",
     studyCourse: undefined,
+    imageLink: "/headshots/Electronics/Enrico Fazzi.jpg"
   },
   {
     name: "Paolo Favero",
     team: Team.BCI,
     status: "Advisor Member",
     studyCourse: undefined,
+    imageLink: "/headshots/BCI/Paolo Favero.jpg"
   },
   {
     name: "Ioana Milea",
     team: Team.Electronics,
     status: "Passive Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Electronics/Ioana Milea.jpg"
   },
   {
     name: "Nele Wunner",
     team: Team.Operations,
     status: "Team Lead",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Nele Wunner.jpg"
   },
   {
     name: "Isabel Tscherniak",
     team: Team.BCI,
     status: "Board Member & Co-Director",
     studyCourse: "M.Sc. Neuroengineering",
+    imageLink: "/headshots/BCI/Isabel Tscherniak.jpg"
   },
   {
     name: "Anna Cernina",
     team: Team.Electronics,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Electronics/Anna Cernina.jpg"
   },
   {
     name: "Tobias Konieczny",
     team: Team.Neuromotion,
     status: "Passive Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Neuromotion/Tobias Konieczny.jpg"
   },
   {
     name: "Daniel Pavlov",
     team: Team.BCI,
     status: "Board Member & Co-Director",
     studyCourse: undefined,
+    imageLink: "/headshots/BCI/Daniel Pavlov.jpg"
   },
   {
     name: "Delfina Luisa Taskin Espinoza",
     team: Team.BCI,
     status: "Advisor Member",
     studyCourse: undefined,
+    imageLink: "/headshots/BCI/Delfina Luisa Taskin Espinoza.jpg"
   },
   {
     name: "Elizabeth Emery",
     team: Team.Operations,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Elizabeth Emery.jpg"
   },
   {
     name: "Ana McWhinnie Fernandez",
     team: Team.BCI,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/BCI/Ana McWhinnie Fernandez.jpg"
   },
   {
     name: "Mert Önder",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Neuromotion/Mert Önder.jpg"
   },
   {
     name: "Doğa Basmacı",
     team: Team.Operations,
     status: "Passive Member",
     studyCourse: undefined,
+    imageLink: "/headshots/Operations/Doğa Basmacı.jpg"
   },
   {
     name: "Thien Le",
     team: Team.BCI,
     status: "Advisor Member",
     studyCourse: "phD student at Medical University Vienna",
+    imageLink: "/headshots/BCI/Thien Le.jpg"
   },
   {
     name: "Fauzi Sholichin",
     team: Team.Operations,
     status: "Member",
     studyCourse: "M. Eng, AI Engineering of Autonomous Systems",
+    imageLink: "/headshots/Operations/Fauzi Sholichin.jpg"
   },
   {
     name: "Pietro Marcolongo",
     team: Team.BCI,
     status: "Member",
     studyCourse: "M.Sc. Bioengineering for Neuroscience",
+    imageLink: "/headshots/BCI/Pietro Marcolongo.jpg"
   },
   {
     name: "Dora Pruteanu",
     team: Team.BCI,
     status: "Member",
     studyCourse: "M. Sc. Computer Science",
+    imageLink: "/headshots/BCI/Dora Pruteanu.jpg"
   },
   {
     name: "Esther Utasá",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: "M. Sc. Neuroengineering",
+    imageLink: "/headshots/Neuromotion/Esther Utasá.jpg"
   },
   {
     name: "Alexandra Khan",
     team: Team.Operations,
     status: "Member",
     studyCourse: "B. Sc. Bioinformatics",
+    imageLink: "/headshots/Operations/Alexandra Khan.jpg"
   },
   {
     name: "Cristoph Müthering",
     team: Team.Operations,
     status: "Member",
     studyCourse: "M. Sc. Biochemistry",
+    imageLink: "/headshots/Operations/Cristoph Müthering.jpg"
   },
   {
     name: "Christian Ritter",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: "M. Sc. Neuroengineering",
+    imageLink: "/headshots/Neuromotion/Christian Ritter.jpg"
   },
   {
     name: "Carl Ziesemer",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: "B. Sc. Management & Technology",
+    imageLink: "/headshots/Neuromotion/Carl Ziesemer.jpg"
   },
   {
     name: "Jorge García Mosquera",
     team: Team.Electronics,
     status: "Member",
     studyCourse: "M. Sc. Neuroengineering",
+    imageLink: "/headshots/Electronics/Jorge García Mosquera.jpg"
   },
   {
     name: "Sining Li",
     team: Team.Operations,
     status: "Member",
     studyCourse: "M. A. Cultural & Cognitive Linguistics",
+    imageLink: "/headshots/Operations/Sining Li.jpg"
   },
   {
     name: "Ahmed Ghaleb",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: "M. Sc. Electrical Engineering",
+    imageLink: "/headshots/Neuromotion/Ahmed Ghaleb.jpg"
   },
   {
     name: "Jan Vizthum",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: "B.Sc., Engineering Science",
+    imageLink: "/headshots/Neuromotion/Jan Vizthum.jpg"
   },
   {
     name: "Katalin Czopf",
     team: Team.BCI,
     status: "Member",
     studyCourse: "M. Sc. Biomedical Engineering & Medical Physics",
+    imageLink: "/headshots/BCI/Katalin Czopf.jpg"
   },
   {
     name: "Begüm Ayaz",
     team: Team.BCI,
     status: "Member",
     studyCourse: "B. Sc. Molekular Biotechnology",
+    imageLink: "/headshots/BCI/Begüm Ayaz.jpg"
   },
   {
     name: "Gabriel Cabrera",
     team: Team.BCI,
     status: "Member",
     studyCourse: "B. Sc. Electrical and Computer Engineering",
+    imageLink: "/headshots/BCI/Gabriel Cabrera.jpg"
   },
   {
     name: "Philipp Wagner",
     team: Team.Neuromotion,
     status: "Member",
     studyCourse: "M.Sc. Robotics, Cognition, Intelligence",
+    imageLink: "/headshots/Neuromotion/Philipp Wagner.jpg"
   },
   {
     name: "Glenn Krafczyk",
     team: Team.BCI,
     status: "Member",
     studyCourse: "M.Sc. Mathematics",
+    imageLink: "/headshots/BCI/Glenn Krafczyk.jpg"
   },
   {
     name: "Solmaz Hüseynli",
     team: Team.Operations,
     status: "Member",
     studyCourse: "B. Sc. Molecular Biotechnology",
+    imageLink: "/headshots/Operations/Solmaz Hüseynli.jpg"
   },
   {
     name: "Sara Gorriz",
     team: Team.BCI,
     status: "Member",
     studyCourse: "M.Sc. Neuroengineering",
+    imageLink: "/headshots/BCI/Sara Gorriz.jpg"
   },
   {
     name: "Tuna Öztürk",
     team: Team.Electronics,
     status: "Member",
     studyCourse: "B. Sc. Electrical engineering & information technology",
+    imageLink: "/headshots/Electronics/Tuna Öztürk.jpg"
   },
 ];
